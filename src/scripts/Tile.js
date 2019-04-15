5893f0806ce59832ec3b05e4e6e965c74fdc4ee8
valentinsejourne <47825129+valentinsejourne@users.noreply.github.com>
2019-04-15 14:26:14 +0200

    Working on Tile Class

--- src/scripts/Tile.js
+++ src/scripts/Tile.js
@@ -1,21 +1,42 @@
 class Tile {
-    
+
     constructor(image, width, height, row, col){
-        //this.image = 
+        this.image = new Image()
+        this.image.src = "dist/res/" + image
         this.width = width
         this.height = height
 
         this.row = row
         this.col = col
 
+        this.srcX = 0
+        this.srcY = 0
+
         this.totalCell = row * col
 
-        this.caseHeight = parseInt(width / col)
-        this.caseWidth  = parseInt(height / row)
+        this.cellHeight = parseInt(width / col)
+        this.cellWidth  = parseInt(height / row)
+
+        this.currentFrame = 0
+        this.canvas = document.querySelector('canvas')
+        this.context = this.canvas.getContext('2d')
+    }
+    updateFrame(){
+      // let cells = new Array()
+      // for (let i = 0; i < this.row; i++) {
+      //   for (let j = 0; j < this.row; j++) {
+      //
+      //   }
+      // }
+      this.currentFrame = this.currentFrame++ % this.totalCell
+      this.srcX = this.currentFrame * this.cellWidth
+      this.srcY = 0
     }
 
-    render(){
-        
+    render(x, y){
+      updateFrame()
+      this.context.drawImage(this.image, this.srcX, this.srcY, this.cellWidth, this.cellHeight, x, y, this.cellWidth, this.cellHeight)
+
     }
 
 }
